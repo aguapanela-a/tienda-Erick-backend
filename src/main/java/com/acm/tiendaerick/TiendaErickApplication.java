58a9c42a7f0f22d6c4d7e5823971e49c7e817257
Erick <eribuit9@gmail.com>
2026-04-03 23:03:19 -0500

    Initial commit: Estructura base de TiendaErick con Spring Boot

---
+++ src/main/java/com/acm/tiendaerick/TiendaErickApplication.java
@@ -0,0 +1,13 @@
+package com.acm.tiendaerick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaErickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaErickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acm/tiendaerick/TiendaErickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acm.tiendaerick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaErickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
