49b824783214e9be754ac077d54f271df2686584
Erick <eribuit9@gmail.com>
2026-04-13 15:51:34 -0500

    Adición de etiquetas para hacer bien un Delete

--- src/main/java/com/acm/tiendaerick/paqueteClientes/repositorio/RepositorioCInvitado.java
+++ src/main/java/com/acm/tiendaerick/paqueteClientes/repositorio/RepositorioCInvitado.java
@@ -3,14 +3,18 @@ package com.acm.tiendaerick.paqueteClientes.repositorio;
 import java.util.Date;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.acm.tiendaerick.paqueteClientes.entidad.EntidadCInvitado;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface RepositorioCInvitado extends JpaRepository<EntidadCInvitado, Long>{
 
+    @Modifying  //Para avisarle a Spring que este metodo es para modificar
+    @Transactional //Para que deje borrar
     @Query(value = "DELETE FROM expiracion_invitado WHERE fecha_expiracion < ?1", nativeQuery=true)
     public void eliminarClienteVencido(Date fecha_expiracion);
 }
