ab1875d68c5641ca1338ac3bef71cc26018fec46
Erick <eribuit9@gmail.com>
2026-04-13 15:14:19 -0500

    Creación de ServicioCRUDCliente

---
+++ src/main/java/com/acm/tiendaerick/paqueteClientes/servicio/ServicioCRUDCliente.java
@@ -0,0 +1,21 @@
+package com.acm.tiendaerick.paqueteClientes.servicio;
+
+import com.acm.tiendaerick.paqueteClientes.dtoCliente.ClienteDTO;
+import com.acm.tiendaerick.paqueteClientes.dtoCliente.ClienteRegistroDTO;
+import com.acm.tiendaerick.paqueteClientes.repositorio.RepositorioCliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioCRUDCliente {
+    private RepositorioCliente repositorioCliente;
+
+    @Autowired
+    public ServicioCRUDCliente(RepositorioCliente repositorioCliente) {
+        this.repositorioCliente = repositorioCliente;
+    }
+
+//    public ClienteDTO registrarCliente(ClienteRegistroDTO registroDTO) {
+//        repositorioCliente.existsEntidadClienteByNombreContainingIgnoreCase(registroDTO.nombre());
+//    }
+}
