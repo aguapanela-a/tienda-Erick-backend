65329ac6c4c632fc89d7a372cc47c027e35aeede
Erick <eribuit9@gmail.com>
2026-04-13 20:36:21 -0500

    Uso de JPQL para sentencias genéricas sin depender a una sola BD

--- src/main/java/com/acm/tiendaerick/paqueteClientes/repositorio/RepositorioCInvitado.java
+++ src/main/java/com/acm/tiendaerick/paqueteClientes/repositorio/RepositorioCInvitado.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.acm.tiendaerick.paqueteClientes.entidad.EntidadCInvitado;
@@ -15,6 +16,7 @@ public interface RepositorioCInvitado extends JpaRepository<EntidadCInvitado, Lo
 
     @Modifying  //Para avisarle a Spring que este metodo es para modificar
     @Transactional //Para que deje borrar
-    @Query(value = "DELETE FROM expiracion_invitado WHERE fecha_expiracion < ?1", nativeQuery=true)
-    public void eliminarClienteVencido(Date fecha_expiracion);
+    //@Query(value = "DELETE FROM expiracion_invitado WHERE fecha_expiracion < ?1", nativeQuery=true)
+    @Query("DELETE FROM EntidadCInvitado e WHERE e.fecha_expiracion < :fecha_actual")
+    public void eliminarClienteVencido(@Param("fecha_actual") Date fecha_actual);
 }
--- src/main/java/com/acm/tiendaerick/paqueteMontos/repositorio/RepositorioMonto.java
+++ src/main/java/com/acm/tiendaerick/paqueteMontos/repositorio/RepositorioMonto.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.acm.tiendaerick.paqueteMontos.entidad.EntidadMonto;
@@ -14,13 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 public interface RepositorioMonto extends JpaRepository<EntidadMonto, Long>{
     
     //Buscar todos los montos de un cliente
-    @Query(value = "SELECT * FROM montos WHERE id_cliente = ?1", nativeQuery=true)  //Query de la consulta
-    public List<EntidadMonto> buscarPorCliente(Long id_cliente);    //Podemos hacer que el tipo sea Page para realizar paginación
+    //@Query(value = "SELECT * FROM montos WHERE id_cliente = ?1", nativeQuery=true)  //Query de la consulta
+    @Query("SELECT montos FROM EntidadMonto montos WHERE montos.cliente.id_cliente = :id_cliente ")
+    public List<EntidadMonto> buscarPorCliente(@Param("id_cliente") long id_cliente);    //Podemos hacer que el tipo sea Page para realizar paginación
 
     //Elimina todos los montos de un cliente
     @Modifying
     @Transactional
-    @Query(value = "DELETE FROM montos WHERE id_cliente = ?1", nativeQuery=true)
-    public void borrarTodosLosMontos(Long id_cliente);
+    //@Query(value = "DELETE FROM montos WHERE id_cliente = ?1", nativeQuery=true)
+    @Query("DELETE FROM EntidadMonto montos where montos.cliente.id_cliente = :id_cliente")
+    public void borrarTodosLosMontos(@Param("id_cliente") long id_cliente);
 
 }
