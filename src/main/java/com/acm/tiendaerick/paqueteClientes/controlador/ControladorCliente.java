8fd7038aedb548e17ca4c0e040aec866b6a1675b
Erick Buitrago <eribuit9@gmail.com>
2026-04-20 17:30:45 -0500

    Se creó el mètodo actualizarCliente en el ControladorCliente

--- src/main/java/com/acm/tiendaerick/paqueteClientes/controlador/ControladorCliente.java
+++ src/main/java/com/acm/tiendaerick/paqueteClientes/controlador/ControladorCliente.java
@@ -4,8 +4,10 @@ import com.acm.tiendaerick.paqueteClientes.dtoCliente.ClienteDTO;
 import com.acm.tiendaerick.paqueteClientes.dtoCliente.ClienteRegistroDTO;
 import com.acm.tiendaerick.paqueteClientes.servicio.OrquestadorCliente;
 import com.acm.tiendaerick.paqueteClientes.servicio.ServicioCliente;
+import com.acm.tiendaerick.paqueteClientes.tipoEnum.TipoCliente;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +25,21 @@ public class ControladorCliente {
     @PostMapping("/registrar")
     public ResponseEntity<ClienteDTO> registrarCliente(ClienteRegistroDTO clienteRegistroDTO){
 
-        //Seleccionar servicio correcto segùn el tipo con el orqeustador
-        ServicioCliente servicioCorrecto = orquestador.seleccionarServicio(clienteRegistroDTO.tipo_cliente());
-
         //Usa el mètodo implementado por el servicio correcto
-        return ResponseEntity.ok(servicioCorrecto.registrarCliente(clienteRegistroDTO));
+        return ResponseEntity.ok(
+                orquestador
+                        .seleccionarServicio(clienteRegistroDTO.tipo_cliente())
+                        .registrarCliente(clienteRegistroDTO)
+        );
+    }
+
+
+    @PutMapping("/atualisar")
+    public ResponseEntity<ClienteDTO> actualizarCliente(ClienteDTO clienteDTO){
+        return ResponseEntity.ok(
+                orquestador.
+                        seleccionarServicio(clienteDTO.tipo_cliente()).
+                        actualizarCliente(clienteDTO)
+        );
     }
 }
